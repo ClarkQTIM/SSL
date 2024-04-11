822ac9410c2f665de953118ab5d63179fcfcb601
clachris <christopher.w.clark@cuanschutz.edu>
2024-04-11 19:55:55 +0000

    Alpine

--- custom_dino/utils.py
+++ custom_dino/utils.py
@@ -467,18 +467,9 @@ def setup_for_distributed(is_master):
 def init_distributed_mode(args):
     # launched with torch.distributed.launch
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
-        print("'RANK' in os.environ and 'WORLD_SIZE' in os.environ")
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
         args.gpu = int(os.environ['LOCAL_RANK'])
-    # launched with submitit on a slurm cluster
-    elif 'SLURM_PROCID' in os.environ:
-        print("'SLURM_PROCID' in os.environ")
-        args.rank = int(os.environ['SLURM_PROCID'])
-        args.gpu = args.rank % torch.cuda.device_count()
-        args.world_size = int(os.environ['SLURM_NTASKS'])
-        os.environ['MASTER_ADDR'] = '127.0.0.1'
-        os.environ['MASTER_PORT'] = '29500'
     # launched naively with `python main_dino.py`
     # we manually add MASTER_ADDR and MASTER_PORT to env variables
     elif torch.cuda.is_available():
@@ -486,11 +477,13 @@ def init_distributed_mode(args):
         args.rank, args.gpu, args.world_size = 0, 0, 1
         os.environ['MASTER_ADDR'] = '127.0.0.1'
         os.environ['MASTER_PORT'] = '29500'
+    # launched with submitit on a slurm cluster
+    elif 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
     else:
         print('Does not support training without GPU.')
         sys.exit(1)
-
-    print(f'rank, gpu, and world_size: {args.rank, args.gpu, args.world_size}')
 
     dist.init_process_group(
         backend="nccl",
